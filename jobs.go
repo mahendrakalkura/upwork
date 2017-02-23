b8bea6f513658f11c6f96bfd8e0f621db19c653b
Mahendra Kalkura <mahendra@kalkura.com>
2017-02-23 16:15:49 +0530

    jobs.go

--- jobs.go
+++ jobs.go
@@ -31,16 +31,51 @@ func jobs(settings *Settings) {
 
 func get_jobs(settings *Settings) []Job {
 	client := get_client(settings)
-	params := map[string]string{
-		"job_status": "open",
-		"paging":     "0;100",
-		"q":          "",
+	parameters := get_parameters()
+	jobs := []Job{}
+	parameters_count := len(parameters)
+	progress_bar := pb.StartNew(parameters_count)
+	for _, params := range parameters {
+		params := map[string]string{
+			"days_posted": "1",
+			"duration":    params.Duration,
+			"job_status":  "open",
+			"job_type":    params.JobType,
+			"paging":      "0;100",
+			"q":           "",
+			"workload":    params.Workload,
+		}
+		_, bytes := search.New(client).Find(params)
+		upwork_jobs := UpworkJobs{}
+		err := json.Unmarshal(bytes, &upwork_jobs)
+		if err != nil {
+			raven.CaptureErrorAndWait(err, nil)
+		}
+		jobs = append(jobs, upwork_jobs.Jobs...)
+		progress_bar.Increment()
 	}
-	_, bytes := search.New(client).Find(params)
-	upwork_jobs := UpworkJobs{}
-	err := json.Unmarshal(bytes, &upwork_jobs)
-	if err != nil {
-		raven.CaptureErrorAndWait(err, nil)
+	progress_bar.Finish()
+	return jobs
+}
+
+func get_parameters() []Parameters {
+	job_types := []string{"hourly", "fixed-price"}
+	durations := []string{"month", "quarter", "semester", "ongoing"}
+	workloads := []string{"full_time"}
+	parameters := []Parameters{}
+	for _, job_type := range job_types {
+		for _, duration := range durations {
+			for _, workload := range workloads {
+				parameters = append(
+					parameters,
+					Parameters{
+						JobType:  job_type,
+						Duration: duration,
+						Workload: workload,
+					},
+				)
+			}
+		}
 	}
-	return upwork_jobs.Jobs
+	return parameters
 }
